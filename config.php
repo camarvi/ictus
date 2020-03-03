bfc4d349b88f4916c6dc69e612342b72c0dd15c1
kamarvi <camarvi24@gmail.com>
2020-03-03 12:42:50 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,21 +3,16 @@
  * DEFINO LAS CONSTANTES QUE UTILIZO EN EL PROGRAMA
  */
 
-// DATOS CONEXON POR MYSQL
-//define("DB_DSN","mysql:host=10.8.65.9;dbname=SUCESOS");
-//define("DB_DSN","mysql:dbname=web");
-   
-
 
 // CONEXION AL SERVIDOR SQL SERVER
 
 
-define("DB_DSN","dblib:host=10.8.65.17;dbname=VISADO");
+define("DB_DSN","dblib:host=xxxxxx;dbname=xxxxx");
 
 
 
-define("DB_USERNAME", "sa");
-define("DB_PASSWORD", "servidor");
+define("DB_USERNAME", "xxx");
+define("DB_PASSWORD", "xxxxx");
 define("PAGE_SIZE", 5);
     
 
